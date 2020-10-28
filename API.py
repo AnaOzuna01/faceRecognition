d5702b1ed3642f87967a3577af4645f98b8eae34
AnaOzuna01 <20175076@ce.pucmm.edu.do>
2020-10-28 17:16:28 -0400

    Upload images

--- API.py
+++ API.py
@@ -1,22 +1,23 @@
-import json
-import uuid
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
+import os
 from faceApp import *
-
-UPLOAD_FOLDER = "/faces"
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, template_folder="template")
+file_name = "faces"
+app.config["IMAGE_UPLOADS"] = os.path.abspath(file_name)
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
-    if request.method == 'POST':
-        file = request.files['file']
-        extension = os.path.splitext(file.filename)[1]
-        f_name = str(uuid.uuid4()) + extension
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
-        return json.dumps({'filename': f_name})
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            print("Image saved")
+            return redirect(request.url)
     return render_template("webApp.html")
 
 
 if __name__ == '__main__':
-   app.run(debug = True, port = 8080)
+    app.run(debug=True, port=8080)
