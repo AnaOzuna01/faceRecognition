a1ce0fb781c902b4ed15c2fa9b88a8ea0c628bfb
AnaOzuna01 <20175076@ce.pucmm.edu.do>
2020-10-28 16:03:21 -0400

    template folder

--- API.py
+++ API.py
@@ -1,52 +1,22 @@
-from flask import Flask, request, redirect, render_template
-from faceApp import *
 import json
-import os
-from werkzeug.utils import secure_filename
+import uuid
+from flask import Flask, request, render_template
+from faceApp import *
 
-IMAGE_UPLOADS = "faces/"
-ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG"]
+UPLOAD_FOLDER = "/faces"
 
+app = Flask(__name__, template_folder="template")
 
-def allowed_image(filename):
-    if not "." in filename:
-        return False
-
-    file_extension = filename.rsplit(".", 1)[1]
-
-    if file_extension.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
-        return True
-    else:
-        return False
-
-
-app = Flask(__name__)
-
-
-
-@app.route("/upload_image", methods=["GET", "POST"])
+@app.route("/", methods=['GET', 'POST'])
 def upload_image():
-    if request.method == "POST":
-
-        if request.files:
-            image = request.files["image"]
-
-            if image.filename == "":
-                print("No filename")
-                return redirect(request.url)
-            if allowed_image(image.filename):
-                filename = secure_filename(image.filename)
-
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-
-            print("Image saved")
-
-            return redirect(request.url)
-        else:
-            print("That file extension is not allowed")
-            return redirect(request.url)
-
+    if request.method == 'POST':
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)[1]
+        f_name = str(uuid.uuid4()) + extension
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+        return json.dumps({'filename': f_name})
     return render_template("webApp.html")
 
 
-app.run(debug=True, port=8080)
+if __name__ == '__main__':
+   app.run(debug = True, port = 8080)
